80d49d08c570b2882ce1891fe3da8c8539ffa30d
Teerapat Inwiset <bluepained.work@gmail.com>
2026-06-21 18:24:27 +0700

    [Update] Grill Cooking

--- Assets/Scripts/Grill/GrillSlot.cs
+++ Assets/Scripts/Grill/GrillSlot.cs
@@ -2,5 +2,20 @@ using UnityEngine;
 
 public class GrillSlot : PlaceSlot
 {
-    
+    [SerializeField] private FoodInteractableObject placedFood;
+
+    public void AssignFood(FoodInteractableObject food)
+    {
+        placedFood = food;
+    }
+
+    public void RemoveFood()
+    {
+        placedFood = null;
+    }
+
+    public void CookTheFood(float heat)
+    { 
+        placedFood?.CookFood(heat);
+    }
 }
